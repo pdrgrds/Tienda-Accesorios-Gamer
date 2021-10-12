8894d771277e164ac7b852b92fe26e3fdec933a2
Leonardo Maldonado <leomaldonadoleo2002@gmail.com>
2021-10-12 14:11:37 -0500

    Creacion del Proyecto SHOPLINE

---
+++ SHOPLINE/src/main/java/org/shopline/ShoplineApplication.java
@@ -0,0 +1,13 @@
+package org.shopline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoplineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoplineApplication.class, args);
+	}
+
+}
