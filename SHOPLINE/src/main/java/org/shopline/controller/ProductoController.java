34417b040c30f25e2fd87b19006e5160245c7742
Leonardo Maldonado <leomaldonadoleo2002@gmail.com>
2021-10-14 00:19:36 -0500

    Proveedor mantenimiento y listado Version 1, Funcionando

--- SHOPLINE/src/main/java/org/shopline/controller/ProductoController.java
+++ SHOPLINE/src/main/java/org/shopline/controller/ProductoController.java
@@ -4,6 +4,7 @@ import org.shopline.repository.ICategoriaRepository;
 import org.shopline.repository.IProveedorRepository;
 import org.shopline.model.Producto;
 import org.shopline.repository.IProductoRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ SHOPLINE/src/main/java/org/shopline/controller/ProveedorController.java
@@ -0,0 +1,41 @@
+package org.shopline.controller;
+
+import org.shopline.model.Proveedor;
+import org.shopline.repository.IProveedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProveedorController {
+@Autowired
+private IProveedorRepository repo;
+
+@GetMapping("/cargarProveedor")
+public String cargarPag(Model model) {
+	model.addAttribute("proveedor", new Proveedor());	
+	return "crudproveedor";
+}
+
+@PostMapping("/grabarProveedor")
+public String grabarPag(@ModelAttribute Proveedor proveedor, Model model) {
+	repo.save(proveedor);	
+	return "crudproveedor";
+}
+
+@GetMapping("/listarProveedor")
+public String listaProducto(Model model) {
+	model.addAttribute("lstProveedor", repo.findAll());	
+	return "listadoproveedor";
+}
+
+@PostMapping("/editarProveedor")
+public String buscarProd(@ModelAttribute Proveedor p, Model model) {
+	model.addAttribute("proveedor", repo.findById(p.getId()));	
+	return "crudproveedor";
+}
+
+}
