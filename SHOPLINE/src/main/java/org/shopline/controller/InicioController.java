2cf3bfac59eb395b01ef6ec2cd617072ceeae1b0
Pedro Grados <pdrgrds@hotmail.com>
2021-11-24 16:21:58 -0500

    Pagina Inicio, banner, Search y Estilos globales

--- SHOPLINE/src/main/java/org/shopline/controller/InicioController.java
+++ SHOPLINE/src/main/java/org/shopline/controller/InicioController.java
@@ -2,13 +2,23 @@ package org.shopline.controller;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.shopline.repository.ICategoriaRepository;
+import org.shopline.repository.IProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.shopline.model.Producto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InicioController {
+	
+	@Autowired
+	private IProductoRepository repoProd;
+	
+	@Autowired
+	private ICategoriaRepository repoCatg;
 
 	@GetMapping("/")
 	public void cargarPrincipal(Model model, HttpServletResponse httpResponse) throws Exception {
@@ -17,6 +27,15 @@ public class InicioController {
 	
 	@GetMapping("/inicio")
 	public String cargarInicio(Model model) {
+		model.addAttribute("listProd", repoProd.findAll());
+		model.addAttribute("listCateg", repoCatg.findAll());
 		return "inicio";
+	}
+	
+	@GetMapping("/buscar")
+	public String cargarBuscar(Model model, @RequestParam int id) {
+		model.addAttribute("listProd", repoProd.findByIdcategoria(id));
+		model.addAttribute("listCateg", repoCatg.findAll());
+		return "search";
 	}
 }
--- SHOPLINE/src/main/java/org/shopline/repository/IProductoRepository.java
+++ SHOPLINE/src/main/java/org/shopline/repository/IProductoRepository.java
@@ -1,5 +1,8 @@
 package org.shopline.repository;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import org.shopline.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IProductoRepository extends JpaRepository<Producto, Integer>{
 
+	ArrayList<Producto> findByIdcategoria(int id);
 }
